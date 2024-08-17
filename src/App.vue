d3018fdf8e25a9742471fed094758036bdcd6322
Ali Hassan <alihassan478@icloud.com>
2024-08-17 18:08:55 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template>
+  <div>
+    HomePage
+    <Button variant="secondary">Click me</Button>
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts">
+import { Button } from "@/components/ui/button";
+</script>
+
+<template><Button>Click me</Button></template>
